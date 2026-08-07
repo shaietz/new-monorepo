a36d67c3e258245cfcc49374e2e83befadd931d7
Shai Etzion <etzshai1@gmail.com>
2026-08-07 14:39:41 +0300

    added syncpack, oxlint, client

---
+++ apps/server/src/index.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+
+const server = fastify({
+  logger: {enabled: true}
+});
+
+server.get("/ping", async (_request, _reply) => {
+  return "pong\n";
+});
+
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    server.log.error(err)
+    process.exit(1);
+  }
+  server.log.info(`Server listening at ${address}`);
+});
